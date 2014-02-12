4ed47d9ee1316c4d2d5293ad3a2fd5aa918805bf
emreiser <emilie.reiser@gmail.com>
2014-02-12 15:19:37 -0500

    Added spec and method to get comics by year

--- lib/ga_example_gem/client.rb
+++ lib/ga_example_gem/client.rb
@@ -3,7 +3,7 @@ require 'httparty'
 require 'ga_example_gem/configuration'
 
 module GaExampleGem
-	
+
 	# This is our client that we can initialize with
 	# GaExampleGem.new
 	class Client
@@ -22,5 +22,9 @@ module GaExampleGem
   		# JSON.parse self.class.get("http://xkcd-unofficial-api.herokuapp.com/xkcd?num=#{number}")
   		JSON.parse HTTParty.get("http://xkcd-unofficial-api.herokuapp.com/xkcd?num=#{number}")
   	end
+
+  	def get_xkcds_from_year(year)
+  		JSON.parse HTTParty.get("http://xkcd-unofficial-api.herokuapp.com/xkcd?year=#{year}")
+		end
 	end
 end
--- spec/ga_example_gem/client_spec.rb
+++ spec/ga_example_gem/client_spec.rb
@@ -10,7 +10,7 @@ describe GaExampleGem::Client do
 
   describe '#get_xkcd' do
   	before do
-      # Intercept an HTTP request that looks like this, 
+      # Intercept an HTTP request that looks like this,
       # and return a specific JSON file instead for the body
       stub_request(:get, 'http://xkcd-unofficial-api.herokuapp.com/xkcd?num=1').
         to_return(body: fixture('single_xkcd.json'))
@@ -22,9 +22,28 @@ describe GaExampleGem::Client do
 
       # Expect that you made a request
       expect(a_request(:get, 'http://xkcd-unofficial-api.herokuapp.com/xkcd?num=1')).to have_been_made
-      
+
       # Check the results of the JSON file/body of HTTP request that we intercepted
       expect(comic.first["title"]).to eq "Barrel - Part 1"
+    end
+  end
+
+  describe '#get_xkcds_from_year' do
+    before do
+      stub_request(:get, 'http://xkcd-unofficial-api.herokuapp.com/xkcd?year=2006').to_return(body: fixture('xkcds_year.json'))
+    end
+
+    it 'fetches all comics from a year when passed year' do
+      year_comics = @client.get_xkcds_from_year(2006)
+
+      expect(a_request(:get, 'http://xkcd-unofficial-api.herokuapp.com/xkcd?year=2006')).to have_been_made
+
+      expect(year_comics.first["safe_title"]).to eq "Barrel - Part 1"
+
+      expect(year_comics.last["safe_title"]).to eq "Island (sketch)"
+
+
+
     end
   end
 end
